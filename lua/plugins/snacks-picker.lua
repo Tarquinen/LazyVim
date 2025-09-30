79e3b7aa2899e487ee9ea87d15c9fad8a2cc4902
Tarquinen <dannysmo@gmail.com>
2025-09-30 14:00:15 -0400

    fix snacks picker to default some searches to normal mode

---
+++ lua/plugins/snacks-picker.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        lsp_references = {
+          on_show = function(picker)
+            -- Focus the list instead of input when picker opens
+            picker:focus("list")
+          end,
+        },
+        recent = {
+          on_show = function(picker)
+            -- Focus the list instead of input when picker opens
+            picker:focus("list")
+          end,
+        },
+        buffers = {
+          on_show = function(picker)
+            -- Focus the list instead of input when picker opens
+            picker:focus("list")
+          end,
+        },
+      },
+    },
+  },
+}
