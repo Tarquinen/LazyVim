3a1d83f3f664c8c993e51c5a524fd062bfc03064
tarqin <dannysmo@gmail.com>
2025-10-17 13:37:04 -0400

    comment cleanup

--- lua/plugins/snacks-picker.lua
+++ lua/plugins/snacks-picker.lua
@@ -22,19 +22,16 @@ return {
         },
         lsp_definitions = {
           on_show = function(picker)
-            -- Focus the list instead of input when picker opens
             picker:focus("list")
           end,
         },
         recent = {
           on_show = function(picker)
-            -- Focus the list instead of input when picker opens
             picker:focus("list")
           end,
         },
         buffers = {
           on_show = function(picker)
-            -- Focus the list instead of input when picker opens
             picker:focus("list")
           end,
         },
