74d7d58a648b74127cb59f90c781f6e8ce409893
Daniel Smolsky <dannysmo@gmail.com>
2025-10-23 09:20:03 -0400

    removed preset overwrites

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,11 +1,7 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    fuzzy = {
-      implementation = "prefer_rust",
-    },
     keymap = {
-      -- preset = "enter",
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
     },
